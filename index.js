8404a3f4b8d806f62320f48f76cbdb182d1d2679
Evan Johnson <evan@cloudflare.com>
2018-12-02 09:29:57 -0800

    Working version! Say hello to folks on Access

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var cookie = require('cookie');
-var cfaccess = require('cfaccess');
+var cfaccessjwt = require('cfaccessjwt');
 
-const kids = [
-  "facb70eaf23a06573eb449601e345b8d2de562fb943de556d2cfda34870f7629",
-  "5504c6480fd61da9b787c2485f38945cd9ff01825391c7b5f56efd9169bb6c44"
-]
 const accessDomain = 'https://ejjio.cloudflareaccess.com';
 const aud = '9d4a635bdcc097213b4474263bb494515713df90575b6efc0ec7ace78e0a30cb';
 
@@ -25,6 +21,9 @@ async function main(r) {
   if (!jwt) {
     return new Response("{}", {statusCode:400})
   }
-  var auth = await cfaccess.Verify(jwt, aud, accessDomain, kids);
-  return new Response(JSON.stringify(auth))
+  // We also need to fetch the JWK keys.
+  var jwkBlob = await fetch(accessDomain + '/cdn-cgi/access/certs')
+  var jwks = await jwkBlob.json()
+  var auth = await cfaccessjwt(jwks, jwt, aud, accessDomain)
+  return new Response('Welcome ' + auth.email)
 }
